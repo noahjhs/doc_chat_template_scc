e775976fefc92b26638eda39ca381d17fea8fbdc
noahhs <noahhs@yahoo.com>
2026-08-23 19:33:44 -0700

    Changed text_area to chat_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,17 @@ from openai import OpenAI
 # for live coding
 #st.session_state.clear()
 
-# Initialize sidebar
-#if 'sidebar_state' not in st.session_state:
-#    st.session_state.sidebar_state = 'expanded'
-
-#st.set_page_config(initial_sidebar_state = st.session_state.sidebar_state)
-
 # Initialize formats
 with open("assets/text.md", "r") as f:
     st.markdown(
         f.read(),
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
 
 # Show title and description.
 st.title("📄 Doc talk")
 st.write(
-    "Provide a document and talk about it. ",
+    "Provide a document. Talk to me about it. ",
 )
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
@@ -42,47 +36,35 @@ with st.sidebar:
         type=("txt", "md"),
     )
 
-# Put away sidebar
-#if uploaded_file and st.session_state.sidebar_state == 'expanded':
-#    st.session_state.sidebar_state = 'collapsed'
-#    st.rerun()
+def answer_them():
 
-def process_and_reset():
-    # Save the typed text into processing variable
-    st.session_state.submitted_text = st.session_state.my_text_area
-    
-    # Immediately clear the widget's internal session state
-    st.session_state.my_text_area = ""
+    # Read the user's document and prompt
+    doc = uploaded_file.read().decode()
+    their_prompt = st.session_state.prompt
 
-# Ask the user for a question via `st.text_area`.
-st.text_area(
-    "Now whaddya wanna know?",
-    placeholder="",
-    disabled=not uploaded_file,
-    key="my_text_area",
-    on_change=process_and_reset,
-)
-
-question = st.session_state.get("submitted_text", "")
-
-if uploaded_file and question:
-
-    # Process the uploaded file and question.
-    document = uploaded_file.read().decode()
-    messages = [
+    # Generate our prompt
+    our_prompt = [
         {
             "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            "content": f"Here's a document: {doc} \n\n---\n\n {their_prompt}",
         }
     ]
 
-    # Generate an answer using the OpenAI API.
-    stream = client.chat.completions.create(
+    # Prompt the model, get its response
+    response = client.chat.completions.create(
         model=model,
-        messages=messages,
+        messages=our_prompt,
         stream=True,
     )
 
-    # Stream the response to the app using `st.write_stream`.
-    st.write_stream(stream)
+    # Show the response
+    st.write_stream(response)
+
+# Prompt the user
+st.chat_input(
+    placeholder="Chat",
+    disabled=not uploaded_file,
+    key="prompt",
+    on_submit=answer_them,
+)
 
